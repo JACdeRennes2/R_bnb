2c1898227ca11646b050c81301d72c806967ccff
clement <clement.pereon@gmail.com>
2023-03-18 18:11:46 +0100

    correction des fautes

--- ui.r
+++ ui.r
@@ -31,24 +31,23 @@ navbarPage(title = "R bnb",
                  p("Le projet R_bnb a pour objectif d'analyser les données d'Airbnb en Europe."),
                  h2("Comment utiliser cette application ?"),
                  p("Choisissez un pays dans le menu déroulant et un moment de la semaine en cliquant sur les boutons radio."),
-                 p("Vous pouvez également ajuster le montant des locations en utilisant le curseur et afficher les valeurs supérieures à 1000 en cochant la case correspondante."),
+                 p("Vous pouvez également ajuster le montant des locations en utilisant le curseur et afficher les valeurs supérieures à 1000 € en cochant la case correspondante."),
                  p("Ensuite, explorez la carte pour voir les locations Airbnb dans le pays choisi."),
                  p("Les locations sont représentées par des marqueurs de couleurs différentes selon le montant de la location."),
-                 p("Cliquez sur un marqueur pour obtenir plus d'informations sur cette location."),
+                 p("Survolez sur un marqueur pour obtenir plus d'informations sur cette location."),
                  plotOutput("carte_acceuil", width = 400, height = 400)
                ),
                column(
                  width = 5,
                  imageOutput('logo'),
-                 h2("Plus d'information "),
-                 p("Le projet R_bnb a pour objectif d'analyser les données d'Airbnb en Europe."),
+                 h2("Plus d'informations"),
                  p("Nos données proviennent du site kaggle : https://www.kaggle.com/datasets/thedevastator/airbnb-prices-in-european-cities"),
-                 p("Nous disposons du prix des locations la semaine et le week-end pour 10 villes européennes.Ainsi que de variables tel que :"),
+                 p("Nous disposons du prix des locations la semaine et le week-end pour 10 grandes villes européennes. Ainsi que de variables telles que :"),
                  p("* Le type de bien"),
-                 p("* Nombre de place dans la chambre"),
-                 p("* Note de propretée"), 
-                 p("* Note des utilisateurs"), 
-                 p("Avec cette aplication Shiny vous pouvez retrouver l'étude que nous avons effectués pour expliquer la différence de prix des biens que l'on observe entre la semaine et le weekend. ")
+                 p("* Le nombre de place dans la chambre"),
+                 p("* La note de propretée"), 
+                 p("* La note des utilisateurs"), 
+                 p("Avec cette aplication Shiny, vous pouvez retrouver l'étude que nous avons effectué pour expliquer la différence de prix des biens que l'on observe entre la semaine et le weekend. ")
                ))
            ),
            tabPanel(title = "Carte",
@@ -74,10 +73,10 @@ navbarPage(title = "R bnb",
                         )
                     )
            ),
-           tabPanel(title = "Stat desc",
+           tabPanel(title = "Statistiques descriptives",
                     fluidPage(
                       fullscreen = TRUE,
-                      titlePanel("Statistique descriptive"), 
+                      titlePanel("Statistiques descriptives"), 
                         plotOutput("host"), 
                         plotOutput("pie", height= 800),
                       plotOutput("prix", height = 1000)
